6ae6a6565e23557652903b56f54f451e02cf148d
Toby Sachs-Quintana <isachsquintana@tesla.com>
2017-10-16 18:31:52 -0700

    finally works

--- charge_calculations.py
+++ charge_calculations.py
@@ -9,42 +9,48 @@ class ChargerPlan(object):
         self.nstops = len(optimized_path)
         self.car_range = 320
         self.charge_rates_kmph = [0] * self.nstops
-        self.remaining_range = [self.car_range] + ([-1] * (self.nstops - 1))
+        self.remaining_range = [self.car_range] + ([-1] * (self.nstops - 2))\
+                                + [self.car_range]
         self.charging_time = [0] * self.nstops
         self.charger_table = table_of_chargers
         self.distances = [0] * self.nstops
         self.path = optimized_path
 
-    def calculate_path_distance(self, subpath):
-        """
-        Calculates the great circle distance along a given subpath
-        Args:
-            charger_table (defaultdict): a subpath of of the shortest path to
-                the destination. The subpath is a list of cities.
-        Returns:
-            dist_to_go (float): km measurement of far to go until the end of 
-                the subpath or when a supercharger is reached
-        """
-        counter = 1
-        dist_to_go = 0
-        for city in subpath[1:]:
-            temp_src = self.charger_table[subpath[counter - 1]]
-            temp_dest = self.charger_table[subpath[counter]]
-            dist_to_go += pn.calculate_distance((temp_src['lat'], temp_src['long']),
-                                                (temp_dest['lat'], temp_dest['long']))
-            counter += 1
-            # if the charger in the destination city is used, return
-            if self.remaining_range[self.path.index(temp_dest)] == 320:
-                return dist_to_go
-        return dist_to_go
-
-    def calculate_km_charge_remaining(self):
-        for idx, range_at_stop_km in enumerate(self.remaining_range):
-            if range_at_stop_km == self.car_range:
-                pass
+    def drive_ok(self, drive_stops):
+        for idx, stop in enumerate(drive_stops):
+            if idx == self.nstops - 2:
+                import ipdb; ipdb.set_trace() # BREAKPOINT
+                if stop['km_on_arrival'] > stop['km_to_next_stop']:
+                    return True
+                else:
+                    stop['charge_time'] = (stop['km_to_next_stop'] - stop['km_on_arrival'])\
+                                            /stop['charge_rate_kmph']
+                    return True
             else:
-                self.remaining_range[idx] = self.remaining_range[idx - 1] -\
-                                                self.distances[idx - 1]
+                next_stop = drive_stops[idx + 1]
+                if stop['charge_here']:
+                    if (stop['charge_rate_kmph'] < next_stop['charge_rate_kmph']) &\
+                        next_stop['charge_here']:
+                        stop['charge_time'] = (stop['km_to_next_stop'] - stop['km_on_arrival'])\
+                                                /stop['charge_rate_kmph']
+                        stop['km_on_departure'] = stop['km_to_next_stop']
+
+                    #if this stop charges faster than the next, charge to the max
+                    elif stop['charge_rate_kmph'] > next_stop['charge_rate_kmph']:
+                        stop['charge_time'] = (self.car_range - stop['km_on_arrival'])\
+                                                /stop['charge_rate_kmph']
+                        stop['km_on_departure'] = self.car_range
+                    elif stop['charge_time'] < 0:
+                        stop['charge_time'] = 0
+                        stop['km_on_departure'] = stop['km_on_arrial']
+                else:
+                    stop['km_on_departure'] = stop['km_on_arrival']
+
+                if stop['km_to_next_stop'] > stop['km_on_departure']:
+                    return False
+                else:
+                    next_stop['km_on_arrival'] = stop['km_on_departure'] - stop['km_to_next_stop']
+        return True
 
     def calculate_time_at_supercharger(self):
         """
@@ -55,35 +61,46 @@ class ChargerPlan(object):
             charger_table (defaultdict): table of 'city' and 'charge_rate_kmph'
         """
         # populate the distance list and charge_rate list
-        for idx, city in enumerate(self.path):
-            self.charge_rates_kmph[idx] = (self.charger_table[city]['charge_rate_kmph'])
+        drive_snapshots = []
+        for idx, city in enumerate(self.path[:-1]):
+            status_at_stop = {'city': city,
+                              'km_on_departure': -1,
+                              'km_on_arrival': -1,
+                              'km_to_next_stop': 0,
+                              'charge_rate_kmph': 0,
+                              'charge_time': 0,
+                              'charge_here': False,
+                              'index': idx}
+            status_at_stop['charge_rate_kmph'] = self.charger_table[city]\
+                                                    ['charge_rate_kmph']
+            if (idx == 0) | (idx == (self.nstops - 1)):
+                status_at_stop['km_on_arrival'] = self.car_range
+                status_at_stop['charge_rate_kmph'] = 0
             temp_src = self.charger_table[self.path[idx]]
             if idx + 1 < self.nstops:
                 temp_dest = self.charger_table[self.path[idx + 1]]
-                self.distances[idx] = pn.calculate_distance((temp_src['lat'],
+            status_at_stop['km_to_next_stop'] = pn.calculate_distance((temp_src['lat'],
                                                                 temp_src['long']),
                                                                (temp_dest['lat'],
                                                                 temp_dest['long']))
-        self.charge_rates_kmph[0] = 0
-        self.charge_rates_kmph[-1] = 0
-        while any(charge < 0 for charge in self.remaining_range):
-            import ipdb; ipdb.set_trace() # BREAKPOINT
-            charge_index = self.charge_rates_kmph.index(max(self.charge_rates_kmph))
-            dist_to_compensate = self.calculate_path_distance(
-                                                              self.path[charge_index:])
-            if dist_to_compensate > self.car_range:
-                dist_to_compensate = self.car_range
-            self.remaining_range[charge_index] = self.car_range
-            self.calculate_km_charge_remaining()
-            self.charging_time[charge_index] = dist_to_compensate / self.charge_rates_kmph[charge_index]
-            self.charge_rates_kmph[charge_index] = 0
-        output_string = self.build_output_string()
-        return self.build_output_string()
+            self.charge_rates_kmph[idx] = (self.charger_table[city]['charge_rate_kmph'])
+            drive_snapshots.append(status_at_stop.copy())
+        top_chargers = sorted(drive_snapshots,
+                              key=lambda k:k['charge_rate_kmph'])
+        while not self.drive_ok(drive_snapshots):
+            charger_to_add = top_chargers.pop()
+            drive_snapshots[charger_to_add['index']]['charge_here'] = True
 
-    def build_output_string(self):
-        output_string = '{}, '.format(self.path[0])
-        for idx, city in enumerate(self.path[1:-1]):
-            middle_part = '{}, {}, '.format(city, self.charging_time[1:-1][idx])
+        import ipdb; ipdb.set_trace() # BREAKPOINT
+        output_string = self.build_output_string(drive_snapshots)
+        return output_string
+
+    def build_output_string(self, drive_info):
+        output_string = '{}, '.format(drive_info[0]['city'])
+        for idx, pitstop in enumerate(drive_info[1:]):
+            print(output_string)
+            middle_part = '{}, {}, '.format(pitstop['city'],
+                                            pitstop['charge_time'])
             output_string = output_string + middle_part
         return output_string + '{}'.format(self.path[-1])
 
